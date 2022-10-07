55f3e3047dc301905355c28c3259b68ff0078707
naghi32 <97885728+naghi32@users.noreply.github.com>
2022-10-07 09:07:12 +0300

    Add files via upload

---
+++ AdjustAuraSpeed/mod.js
@@ -0,0 +1,8 @@
+const skillsFileName = 'global\\excel\\skills.txt';
+const skills = D2RMM.readTsv(skillsFileName);
+skills.rows.forEach((row) => {
+  if (row.perdelay !== '') {
+    row.perdelay = config.perdelay;
+  }
+});
+D2RMM.writeTsv(skillsFileName, skills);
